863d9da2ac3ad048bc2242c007f3090230bd637e
IndayaraSilva <indayarasilva@gmail.com>
2022-06-08 22:16:17 -0300

    Atualizações

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function botao1(){
 
 
-    var addTexto = document.getElementById("texto1")
-    var Paragrafo = document.createElement("p")
-
-
+    var addTexto1 = document.getElementById("texto1")
+    var Paragrafo1 = document.createElement("p")
+    
+    Paragrafo1.innerHTML = addTexto1.value
+    document.body.append(Paragrafo1);
 }
 
 function botao2(){
 
-  var addTexto = document.getElementById("texto1")
-  var Paragrafo = document.createElement("p")
-  Paragrafo.innerHTML = addTexto.value
+  var addTexto2 = document.getElementById("texto2")
+  var Paragrafo2 = document.createElement("p")
+  Paragrafo2.innerHTML = addTexto2.value
 
+
+  document.body.append(Paragrafo2);
+
+//"nessa parte podemos podemos substituir body por section(não sei se da certo)"
 }
