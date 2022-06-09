cb9343d66259a7d759974c810f548953c7faed06
IndayaraSilva <indayarasilva@gmail.com>
2022-06-08 21:56:52 -0300

    Estrutura HTML e primeiras estilizações
    Co-authored-by: Augusto Deolindo <AugustoADeolindo@users.noreply.github.com>
    Co-authored-by: Leandro Elias <oshirowiski0802@users.noreply.github.com>
    Co-authored-by: CarlosKSS <CarlosKSS@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+function botao1(){
+
+
+    var addTexto = document.getElementById("texto1")
+    var Paragrafo = document.createElement("p")
+
+
+}
+
+function botao2(){
+
+  var addTexto = document.getElementById("texto1")
+  var Paragrafo = document.createElement("p")
+  Paragrafo.innerHTML = addTexto.value
+
+}
